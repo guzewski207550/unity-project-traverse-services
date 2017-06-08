678443ef25fad74c5dea7d5d723d9d74924eddea
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-06-08 15:40:18 +0200

    Uploac clear project from https://start.spring.io/.

---
+++ src/main/java/com/example/makolab/unity/traverse/TraverseApplication.java
@@ -0,0 +1,12 @@
+package com.example.makolab.unity.traverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraverseApplication.class, args);
+	}
+}
